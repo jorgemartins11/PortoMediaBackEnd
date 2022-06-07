76e0bd98a99d8dacd61681500f7371094af58557
Jorge Daniel <jorge.daniel11@outlook.com>
2022-06-07 10:47:50 +0100

    contact route

--- routes/contacts.routes.js
+++ routes/contacts.routes.js
@@ -7,7 +7,15 @@ const {
 
 const contactsController = require('../controllers/contacts.controller');
 
-router.route('/').post(function (req, res) {
+router.route('/').post([
+    body("name").notEmpty(),
+    body("email", "O email que inseriu não é valido!").isEmail().notEmpty(),
+    body("contact").isNumeric().optional().isLength({
+        min: 9,
+        max: 9
+    }).withMessage("O contacto que inseriu não é válido!"),
+    body("message").notEmpty()
+], function (req, res) {
     const errors = validationResult(req)
     if (errors.isEmpty()) {
         contactsController.makeContact(req, res);
