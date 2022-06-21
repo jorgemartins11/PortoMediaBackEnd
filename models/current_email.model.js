6e3412f4e1f290e9b11a96d357a829384f80c999
Jorge Daniel <jorge.daniel11@outlook.com>
2022-06-21 12:17:32 +0100

    d

--- models/current_email.model.js
+++ models/current_email.model.js
@@ -1,8 +1,12 @@
 const dbConfig = require('../config/db.config');
 
-const { Sequelize, Model, DataTypes } = require('sequelize'); 
+const {
+    Sequelize,
+    Model,
+    DataTypes
+} = require('sequelize');
 const sequelize = new Sequelize.Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST, 
+    host: dbConfig.HOST,
     dialect: dbConfig.dialect,
     retry: {
         match: [
@@ -30,10 +34,15 @@ class CurrentEmail extends Model {}
 
 CurrentEmail.init({
     email: DataTypes.STRING
-}, { sequelize, modelName: 'current_email'})
+}, {
+    timestamps: false
+}, {
+    sequelize,
+    modelName: 'current_email'
+})
 
 sequelize.sync().then().catch(error => {
-    console.log("ERROR: " + error + " SYNC CURRENT EMAIL MODEL"); 
+    console.log("ERROR: " + error + " SYNC CURRENT EMAIL MODEL");
 })
 
 exports.CurrentEmail = CurrentEmail;
